416d02e2f7bf3b389c38bd5b469ff110269d653d
Tsche <Tsche@users.noreply.github.com>
2023-02-23 23:27:32 +0100

    remove need for metaclass

--- python/reverse_prefix.py
+++ python/reverse_prefix.py
@@ -1,13 +1,11 @@
-class R(type):
-    def __getitem__(cls, args):
-        ''' enables this prefix for lists '''
-        return [*reversed(args)]
-
-
-class r(metaclass=R):
+class r:
     def __new__(cls, *args):
         ''' enables this prefix for tuples '''
         return tuple(reversed(args))
+
+    def __class_getitem__(cls, args):
+        ''' enables this prefix for lists '''
+        return [*reversed(args)]
 
 
 assert r[1, 2, 3, 4] == [4, 3, 2, 1]
